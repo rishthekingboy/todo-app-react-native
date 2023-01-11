6107c6865504538a4e819145072c5ab30cfa866e
Rishabh Singh <rishmark98@gmail.com>
2023-01-11 13:14:18 +0530

    gif added

--- App.js
+++ App.js
@@ -4,16 +4,9 @@ import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { TodoContext } from "./src/context";
 import BottomTabNavigator from "./src/navigation/BottomTabNavigator ";
-const items = [
-  { name: "task 1", id: 1, isPending: false },
-  { name: "task 1", id: 2, isPending: false },
-  { name: "task 1", id: 3, isPending: false },
-  { name: "task 1", id: 4, isPending: true },
-  { name: "task 1", id: 5, isPending: false },
-  { name: "task 1", id: 6, isPending: false },
-];
+
 export default function App() {
-  const [todos, setTodos] = useState([...items]);
+  const [todos, setTodos] = useState([]);
   return (
     <TodoContext.Provider value={{ todos, setTodos }}>
       <View style={styles.container}>
